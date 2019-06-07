1c2a6c1c8614f9c6af4caff5210b17c7e07dc72c
Salesforce Casts <teja@salesforcecasts.com>
2019-06-08 01:14:17 +0530

    test

--- force-app/main/default/lwc/exploreFolderStructure/exploreFolderStructure.js
+++ force-app/main/default/lwc/exploreFolderStructure/exploreFolderStructure.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class ExploreFolderStructure extends LightningElement {}
+export default class ExploreFolderStructure extends LightningElement {
+    greeting = 'world';
+    handleChange(event) {
+        this.greeting = event.target.value;
+        // eslint-disable-next-line no-console
+        console.log(this.greeting);
+    }
+}
