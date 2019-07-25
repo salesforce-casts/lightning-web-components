0f80dfc3487f54b4c5f651867517457975a4c16d
Salesforce Casts <teja@salesforcecasts.com>
2019-07-26 00:05:51 +0530

    completed explore LDS Hanlders

--- force-app/main/default/lwc/exploreLDSHandlers/exploreLDSHandlers.js
+++ force-app/main/default/lwc/exploreLDSHandlers/exploreLDSHandlers.js
@@ -3,6 +3,17 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 export default class ExploreLDSHandlers extends LightningElement {
     @api recordId
 
+    handleSubmit(event){
+        //1. Prevent the default behaviour 
+        event.preventDefault();
+        //2. Get all the fields of the record
+        const fields = event.detail.fields;
+        //3. Modify a field 
+        fields.Name = 'Testing Salesforce Casts';
+        //4. Submit the form 
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
+    }
+
     handleSuccess(event){
         // eslint-disable-next-line no-console
         console.log(event.detail);
