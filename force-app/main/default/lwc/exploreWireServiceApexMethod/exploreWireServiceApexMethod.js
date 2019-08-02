afdf07d116f902451f7217211958ee4d3ab416a1
Salesforce Casts <teja@salesforcecasts.com>
2019-08-03 00:43:55 +0530

    Completed invoking apex method imperatively

--- force-app/main/default/lwc/exploreWireServiceApexMethod/exploreWireServiceApexMethod.js
+++ force-app/main/default/lwc/exploreWireServiceApexMethod/exploreWireServiceApexMethod.js
@@ -6,16 +6,35 @@ export default class ExploreWireServiceApexMethod extends LightningElement {
 
     @track accounts;
     @track error;
-
-    @wire(getAccountsList)
-    wiredAccounts({error, data}){
-        if(data){
-            this.accounts = data;
-            this.error = error;
-        } else if(error){
-            this.error = error;
-            this.accounts = undefined;
-        }
+    //1. Wiring an adaptor(getRecord) to a property
+    //2. Wiring an Apex method to a property
+    //3. Wiring an apex method back to a function
+    
+    renderedCallback(){
+        getAccountsList()
+            .then(result => {
+                this.accounts = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
     }
+    
+    
+    
+    
+    // @wire(getAccountsList)
+    // wiredAccounts({error, data}){
+    //     if(data){
+    //         this.accounts = data;
+    //         this.error = error;
+    //     } else if(error){
+    //         this.error = error;
+    //         this.accounts = undefined;
+    //     }
+    // }
+
+
+
 
 }
