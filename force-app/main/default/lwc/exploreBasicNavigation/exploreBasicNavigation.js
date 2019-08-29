06e6e89a8139af30f37aa376d49fdef379c162c9
Salesforce Casts <teja@salesforcecasts.com>
2019-08-30 02:26:28 +0530

    Basic Navigation completed

--- force-app/main/default/lwc/exploreBasicNavigation/exploreBasicNavigation.js
+++ force-app/main/default/lwc/exploreBasicNavigation/exploreBasicNavigation.js
@@ -9,9 +9,6 @@ export default class ExploreBasicNavigation extends NavigationMixin(LightningEle
             attributes: {
                 objectApiName: "Case",
                 actionName: "home"
-            },
-            state: {
-                "tingtong": "chungchong"
             }
         };
 
